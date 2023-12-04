a898b685ec635023b8a9c1e7e0bf5c059fc72ebb
longestcow <83398131+longestcow@users.noreply.github.com>
2023-12-04 15:26:59 -0600

    basic foundation
    Added basic logic for survival, birth, death (doesn't work though, should work once neighbourcount is implemented)

--- sketch.js
+++ sketch.js
@@ -1,13 +1,19 @@
-let canvasSize = 800, cellSize = 80;
-let grid = [];
 
-let rules = [[6], [6], 5, 0]; // birthNeighbour, survivalNeighbour, deathFadeRate, neighbourMode (0=Moore, 1=Von Neumann)
+let canvasSize = 800, cellSize = 80;
+let grid = [],tGrid;
+
+let rules = [[0,6], [0,6], 5, 0]; // birthNeighbour, survivalNeighbour, deathFadeRate, neighbourMode (0=Moore, 1=Von Neumann)
 let cMode = 1; // color mode (0 = stage, 1=dist from centre)
 function setup() {
+  frameRate(30);
   for(let i = 0; i<canvasSize/cellSize; i++){
-    grid.push(new Array(canvasSize/cellSize));
+    grid.push(new Array());
     for(let j = 0; j<canvasSize/cellSize; j++){
-      grid[i].push(new Array(canvasSize/cellSize));
+      grid[i].push(new Array());
+      for(let k = 0; k<canvasSize/cellSize; k++){
+        let chance = round(random(1,50))===1;
+        grid[i][j].push(new Cell((chance)?rules[2]:0, (chance)?[random(255),random(255),random(255)]:[0]));
+      }
     }
   }
   createCanvas(canvasSize,canvasSize,WEBGL);
@@ -17,25 +23,44 @@ function setup() {
 
 function draw() {
   background(220);
-  fill(0,0,0,0);
+  let i,j,k,cCell;
+  tGrid=structuredClone(grid);
   for(let x = 0; x<width; x+=cellSize){
+    i=x/cellSize;
     for(let y = 0; y<height; y+=cellSize){
+      j=y/cellSize;
       for(let z = 0; z<height; z+=cellSize){
-        
+        k=z/cellSize;
+        cCell=grid[i][j][k];
+
+        if(cCell.stage!==0){
+          if(!rules[0].includes(cCell.neighbourCount())){
+            tGrid[i][j][k]=new Cell(rules[2]-1,cCell.color,true);
+          }
+          else tGrid[i][j][k]=new Cell(cCell.stage, cCell.color);
+
+        }
+        else if(rules[0].includes(cCell.neighbourCount())){
+          tGrid[i][j][k]=new Cell(rules[2],[random(255),random(255),random(255)]);
+        }
+        if(cCell.dying){cCell.stage-=(cCell.stage===0)?0:1;}
         push();
         translate(x,y,z);
+        fill(cCell.color);
         box(cellSize);
         pop();
       }
     }
   }
+  grid=tGrid;
   orbitControl(2,2,1);
 }
 
 class Cell{
-  constructor(stage, color){//if stage == 0, dead
+  constructor(stage, color, dying=false){//if stage == 0, dead
     this.stage=stage;
     this.color=color;
+    this.dying=dying;
   }
 
   neighbourCount(){
