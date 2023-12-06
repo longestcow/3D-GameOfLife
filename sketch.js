3839c0545bea67e150032c92006c4ab9b8c00a4a
longestcow <83398131+longestcow@users.noreply.github.com>
2023-12-06 15:23:05 -0600

    getting cells to show
    the cells show now, but no interaction yet

--- sketch.js
+++ sketch.js
@@ -1,24 +1,32 @@
 
 let canvasSize = 800, cellSize = 80;
 let grid = [],tGrid;
-
-let rules = [[0,6], [0,6], 5, 0]; // birthNeighbour, survivalNeighbour, deathFadeRate, neighbourMode (0=Moore, 1=Von Neumann)
+let _size = canvasSize/cellSize;
+let rules = [[6], [1], 5, 0]; // birthNeighbour, survivalNeighbour, deathFadeRate, neighbourMode (0=Moore, 1=Von Neumann)
 let cMode = 1; // color mode (0 = stage, 1=dist from centre)
+let cX=_size/2,cY=_size/2,cZ=_size/2;
+let maxDist;
 function setup() {
   frameRate(30);
-  for(let i = 0; i<canvasSize/cellSize; i++){
+  let temp=[];
+  temp=temp.sort();
+  maxDist=temp[temp.length-1];
+  for(let i = 0; i<_size; i++){
     grid.push(new Array());
-    for(let j = 0; j<canvasSize/cellSize; j++){
+    for(let j = 0; j<_size; j++){
       grid[i].push(new Array());
-      for(let k = 0; k<canvasSize/cellSize; k++){
+      for(let k = 0; k<_size; k++){
         let chance = round(random(1,50))===1;
-        grid[i][j].push(new Cell((chance)?rules[2]:0, (chance)?[random(255),random(255),random(255)]:[0]));
+        temp.push(distFromCentre(i,j,k));
+        grid[i][j].push(new Cell((chance)?rules[2]:0, (chance)?color(255, map(distFromCentre(i,j,k),0,maxDist,0,255), 0):color(0,0,0,0)));
       }
     }
   }
+
+  
   createCanvas(canvasSize,canvasSize,WEBGL);
   camera(-canvasSize,-canvasSize,-canvasSize);
-  strokeWeight(0.5);
+  strokeWeight(0);
 }
 
 function draw() {
@@ -33,22 +41,30 @@ function draw() {
         k=z/cellSize;
         cCell=grid[i][j][k];
 
-        if(cCell.stage!==0){
-          if(!rules[0].includes(cCell.neighbourCount())){
+        if(cCell.stage!==0){ //alive
+          if(!rules[0].includes(cCell.neighbourCount)){//did not survive
             tGrid[i][j][k]=new Cell(rules[2]-1,cCell.color,true);
           }
-          else tGrid[i][j][k]=new Cell(cCell.stage, cCell.color);
+          push();
+          translate(x,y,z);
+          fill(cCell.color);
+          box(cellSize);
+          pop();
+        }
+        else if(rules[0].includes(cCell.neighbourCount)){
+          
+          tGrid[i][j][k]=new Cell(rules[2],color(random(255),random(255),random(255)));
+        }
+        else {
+          tGrid[i][j][k]=new Cell(0, cCell.color);
+          if(cCell.dying){
+            cCell.dying=false;
+          }
+        }
+        if(cCell.dying){
+          cCell.stage-=cCell.stage===0?0:1;
+        }
 
-        }
-        else if(rules[0].includes(cCell.neighbourCount())){
-          tGrid[i][j][k]=new Cell(rules[2],[random(255),random(255),random(255)]);
-        }
-        if(cCell.dying){cCell.stage-=(cCell.stage===0)?0:1;}
-        push();
-        translate(x,y,z);
-        fill(cCell.color);
-        box(cellSize);
-        pop();
       }
     }
   }
@@ -61,15 +77,14 @@ class Cell{
     this.stage=stage;
     this.color=color;
     this.dying=dying;
+    this.neighbourCount=0;
   }
 
-  neighbourCount(){
-    return 0; // neighbour count
-  }
+}
 
-  distFromCentre(){
-    return 0; // distance from centre
-  }
-
-
+function distFromCentre(x,y,z){
+  return Math.pow((Math.pow(cX - x, 2) + 
+  Math.pow(cY - y, 2) + 
+  Math.pow(cZ - z, 2) * 1.0), 0.5);
+  
 }
