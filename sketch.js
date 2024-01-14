f238a3ca0d59a417389787bdf68fc53201d6b323
mambo <omiyer76@gmail.com>
2024-01-14 13:22:04 -0600

    code refactor

--- sketch.js
+++ sketch.js
@@ -1,37 +1,38 @@
 
-let canvasSize = 800, cellSize = 40;
-let grid = [],tGrid,neighbours=[];
+let canvasSize = 800, cellSize = 36;
+let grid = [],neighbours=[];
 let _size = canvasSize/cellSize;
-//let rules = [[1,2,3,4,5,6], [3], [1], true]; // birthNeighbour, survivalNeighbour, deathFadeRate, neighbourMode (true=Moore, false=Von Neumann)
-let rules = [[1], [1,2,3], 10, false]; // birthNeighbour, survivalNeighbour, deathFadeRate, neighbourMode (true=Moore, false=Von Neumann)
+let rules = [[2,4,5,6,7,8,9,10,11,12,13,14,15,21,22,23,24,25,26], [1,2,3,8,6,7,8,5,4,12,13,15], 2, false]; // birthNeighbour, survivalNeighbour, deathFadeRate, neighbourMode (true=Moore, false=Von Neumann)
+//let rules = [[1], [1,2,3], 10, false]; // birthNeighbour, survivalNeighbour, deathFadeRate, neighbourMode (true=Moore, false=Von Neumann)
 let cMode = 1; // color mode (0 = stage, 1=dist from centre)
 let cX=_size/2,cY=_size/2,cZ=_size/2;
-let maxDist;
 function setup() {
-  frameRate(30);
-  maxDist=distFromCentre(0,0,0);
-  for(let i = 0; i<_size; i++){
+  print(_size)
+  frameRate(24);
+  let maxDist=distFromCentre(0,0,0);
+  for(let i = 0; i<_size; i++){ //fill neighbours array
     neighbours.push(new Array());
     for(let j = 0; j<_size; j++){
       neighbours[i].push(new Array());
       for(let k = 0; k<_size; k++)
         neighbours[i][j].push(0);
-      
     }
   }
-    for(let i = 0; i<_size; i++){
-      grid.push(new Array());
-      for(let j = 0; j<_size; j++){
-        grid[i].push(new Array());
-        for(let k = 0; k<_size; k++){
-          let chance = (i===_size/2 && j===_size/2 && k===_size/2);
-          // let chance = (round(random(1,40)) === 1)
-          grid[i][j].push(new Cell((chance)?rules[2]:0,color(map(distFromCentre(i,j,k),0,maxDist,0,125), 0, map(distFromCentre(i,j,k),0,maxDist,0,255))));
-          if(chance)updateNeighbours(i,j,k,true);
-        }
+  for(let i = 0; i<_size; i++){
+    grid.push(new Array());
+    for(let j = 0; j<_size; j++){
+      grid[i].push(new Array());
+      for(let k = 0; k<_size; k++){
+        let chance = (abs(i-_size/2)<3 && abs(j-_size/2)<3 && abs(k-_size/2)<3);
+        // let chance = (round(random(1,20)) === 1)
+        grid[i][j].push(
+          new Cell((chance)?rules[2]:0,
+          color(map(distFromCentre(i,j,k),0,maxDist,0,125), 0, map(distFromCentre(i,j,k),0,maxDist,0,255)))
+        );
+        if(chance)updateNeighbours(neighbours,i,j,k,true);
       }
+    }
   }
-//q5js
   
   createCanvas(canvasSize,canvasSize,WEBGL);
   camera(-canvasSize,-canvasSize,-canvasSize);
@@ -42,42 +43,40 @@ function draw() {
   background(220);
   let i,j,k,cCell;
   let tGrid = structuredClone(grid);
-  let tNieghbours = structuredClone(neighbours);
-  for(let x = 0; x<width; x+=cellSize){
-    i=x/cellSize;
-    for(let y = 0; y<height; y+=cellSize){
-      j=y/cellSize;
-      for(let z = 0; z<height; z+=cellSize){
-        k=z/cellSize;
+  let tNeighbours = structuredClone(neighbours);
+  for(let i = 0; i<_size; i++){
+    for(let j = 0; j<_size; j++){
+      for(let k = 0; k<_size; k++){
         cCell=grid[i][j][k];
           if(cCell.stage!==0){ //alive
             push();
-            translate(x,y,z);
+            translate(i*cellSize,j*cellSize,k*cellSize);
             fill(cCell.color);
             box(cellSize);
             pop();
           }
 
-         //nextgen stuff
-          let n = neighbours[i][j][k];
-          if(cCell.stage!==0){ //alive
-            if(rules[1].includes(n))//did survive
-              tGrid[i][j][k]=new Cell(cCell.stage,cCell.color);
-            else {
-              tGrid[i][j][k]=new Cell(cCell.stage-1, cCell.color);
-              if(cCell.stage-1==0)updateNeighbours(i,j,k,false);
-            }
+        //nextgen stuff
+        let n = neighbours[i][j][k];
+        if(cCell.stage!==0){ //alive
+          if(cCell.stage!==rules[2] || !rules[1].includes(n)){//alive stays/starts dying
+            tGrid[i][j][k]=new Cell(cCell.stage-1, cCell.color);
+            if(cCell.stage-1==0)updateNeighbours(tNeighbours,i,j,k,false);//alive becomes dead
           }
-          else if(rules[0].includes(n)){//can be brought back to life
-            tGrid[i][j][k]=new Cell(rules[2],cCell.color);
-            updateNeighbours(i,j,k,true);
-          }
-          else //dead stays dead
-            tGrid[i][j][k]=new Cell(0, cCell.color);
+          else if(rules[1].includes(n))//alive stays alive
+            tGrid[i][j][k]=new Cell(cCell.stage,cCell.color);
+        }
+        else if(rules[0].includes(n)){//dead becomes alive
+          tGrid[i][j][k]=new Cell(rules[2],cCell.color);
+          updateNeighbours(tNeighbours,i,j,k,true);
+        }
+        else //dead stays dead
+          tGrid[i][j][k]=new Cell(0, cCell.color);
 
       }
     }
   }
+  neighbours=tNeighbours;
   grid=tGrid;
   orbitControl(2,2,1);
 }
@@ -88,8 +87,6 @@ class Cell{
     this.stage=stage;
     this.color=color;
   }
-
-
 }
 
 function distFromCentre(x,y,z){
@@ -101,14 +98,14 @@ function distFromCentre(x,y,z){
   
 }
 
-function updateNeighbours(x,y,z,alive){
+function updateNeighbours(neighb,x,y,z,alive){
   for(let i = -1; i<=1; i++){
     for(let j = -1; j<=1; j++){
       for(let k = -1; k<=1; k++){
         if(!rules[3] && (abs(i)+abs(j)+abs(k)>1))continue; //von neumann
         if(i==0 && j==0 && k==0)continue;//self state
         try{
-        neighbours[x+i][y+j][z+k]+=(alive)?1:-1;
+        neighb[x+i][y+j][z+k]+=(alive)?1:-1;
         } catch(e){}
       }
     }
